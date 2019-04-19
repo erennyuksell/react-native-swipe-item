a6891de546fb21f03a26cdff49dff29a54f1bc22
Frekansapp <43651689+Frekansapp@users.noreply.github.com>
2019-04-19 15:22:32 +0300

    Delete index.js

--- dist/component/index.js
+++
@@ -1,8 +0,0 @@
-// @flow
-import SwipeItem from './component/swipeItem';
-import SwipeButtonsContainer from './component/swipeButtonsContainer';
-
-export {
-    SwipeItem,
-    SwipeButtonsContainer
-}
